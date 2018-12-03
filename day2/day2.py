16ae0be986800fc78c578f635184c246e3cb1528
owen32497 <32204846+owen32497@users.noreply.github.com>
2018-12-02 19:23:07 -0500

    Implimented part two

--- day2/day2.py
+++ day2/day2.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
+# Author: rap7or22
+# 12/2/2018
 from collections import Counter
 
 
 def partOne():
     twos = 0
     threes = 0
-    file = open('input', 'r')
+    file = open('input').read().splitlines()
     for line in file:
         counts = Counter(Counter(line).values())
         if 2 in counts:
@@ -15,21 +17,18 @@ def partOne():
 
     print("Checksum:", twos*threes)
 
-def p1():
-    twos = 0
-    threes = 0
-    file = open('input', 'r')
-    for line in file:
-        counts = Counter(line).values()
-        if 2 in counts:
-            twos += 1
-        elif 3 in counts:
-            threes += 1
-    
-    print("Checksum:", twos*threes)
+def partTwo():
+    file = open('input').read().splitlines()
+    for line1 in file:
+        for line2 in file:
+            difference = [i for i,x in zip(line1,line2) if i == x]
+            if len(line1)-len(difference) == 1:
+                print("".join(difference))
+                return 1
+   
 def main():
     partOne()
-    p1()
-    return 1
+    partTwo()
+    exit()
 
 main()
