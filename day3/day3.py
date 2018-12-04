44942985004c6fd533736c047a316c3423c329d9
owen32497 <32204846+owen32497@users.noreply.github.com>
2018-12-03 23:43:17 -0500

    saved challenge text, and solved day3

--- day3/day3.py
+++ day3/day3.py
@@ -1,1 +1,45 @@
-#!/usr/share/python3
+#!/usr/bin/python3
+import numpy
+
+def partOne():
+    file = open('input').read().splitlines()
+    values= []
+    canvas = numpy.zeros((1000,1000))
+    
+    for line in file:
+        num = int(line.split(' ')[0][1:])
+        x,y = int(line.split(' ')[2].split(',')[0]), int(line.split(' ')[2].split(',')[1].split(':')[0])
+        w,h = int(line.split(' ')[3].split('x')[0]), int(line.split(' ')[3].split('x')[1])
+        values.append([num, x, y, w, h])
+    for i, x, y, w, h in values:
+        canvas[x:x+w, y:y+h] += 1
+    
+    print(numpy.sum(canvas>1))
+
+def PartTwo():
+
+    file = open('input').read().splitlines()
+    values= []
+    canvas = numpy.zeros((1000,1000))
+    
+    for line in file:
+        num = int(line.split(' ')[0][1:])
+        x,y = int(line.split(' ')[2].split(',')[0]), int(line.split(' ')[2].split(',')[1].split(':')[0])
+        w,h = int(line.split(' ')[3].split('x')[0]), int(line.split(' ')[3].split('x')[1])
+        values.append([num, x, y, w, h])
+    
+    for i, x, y, w, h in values:
+        canvas[x:x+w, y:y+h] += 1
+
+    for i, x, y, w, h in values:
+        if numpy.all(canvas[x:x+w, y:y+h] == 1):
+            print(i)
+            break
+
+
+def main():
+    partOne()
+    PartTwo()
+    
+
+main()
